c52d07b8437e1fa197c55dbc366059a9a96b051e
Mathieu Geukens <mr-zwets@protonmail.com>
2025-02-23 18:10:19 +0100

    fix style mobile

--- src/components/SiteBanner.vue
+++ src/components/SiteBanner.vue
@@ -8,32 +8,32 @@ const store = useStore()
 
 <template>
   <header style="margin-bottom: 25px">
-    <div style="display: flex; align-items: center;" >
+    <div style="display: flex; align-items: center;" class="topBar">
       <RouterLink class="siteName" to="/">
         <h1>BCH Hodl Dapp 💎</h1>
       </RouterLink>
-      <div style="margin-left: 150px;">
+      <div class="navContainer">
         <nav style="display: flex; gap: 20px;">
           <RouterLink to="/">All Contracts</RouterLink>
           <RouterLink to="/my-contracts">User Contracts</RouterLink>
           <RouterLink to="/create-contract">Create Contract</RouterLink>
         </nav>
       </div>
-      <div class="addressInfo" style="margin-left: 150px;">
-      <div class="balance">
-        <span>
-          <wcButton />
-        </span>
-        <span style="margin-left: 20px; width: 220px;" v-if="store.userAddress">
-          Balance: <span v-if="store.bchBalance !== undefined">
-            <span>{{ satsToBchAmount(Number(store.bchBalance)).toFixed(2) }} BCH </span>
-          </span><span v-else> loading...</span>
-        </span>
+      <div class="wcContainer">
+        <div class="balance">
+          <span>
+            <wcButton />
+          </span>
+          <span style="margin-left: 20px; width: 220px;" v-if="store.userAddress">
+            Balance: <span v-if="store.bchBalance !== undefined">
+              <span>{{ satsToBchAmount(Number(store.bchBalance)).toFixed(2) }} BCH </span>
+            </span><span v-else> loading...</span>
+          </span>
+        </div>
+        <div class="address" v-if="store.userAddress">
+          {{ store.userAddress.slice(0,28) + "..." + store.userAddress.slice(-10)}}
+        </div>
       </div>
-      <div class="address" v-if="store.userAddress">
-        {{ store.userAddress.slice(0,28) + "..." + store.userAddress.slice(-10)}}
-      </div>
-    </div>
     </div>
     <h3>A smart contract to timelock your Bitcoin Cash!</h3>
   </header>
@@ -43,5 +43,35 @@ const store = useStore()
 a {
   color: black;
   padding: 0;
+}
+.navContainer{
+  margin-left: 150px;
+}
+.wcContainer{
+  margin-left: 150px;
+}
+@media only screen and (max-width: 1300px) {
+  .topBar {
+    justify-content: space-between;
+  }
+  .navContainer {
+    margin-left: 0px;
+  }
+  .wcContainer {
+    margin-left: 0px;
+  }
+}
+@media only screen and (max-width: 1000px) {
+  .topBar {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .navContainer {
+    margin-left: 0;
+  }
+  .wcContainer{
+    margin-left: 0px;
+    margin: 10px 0;
+  }
 }
 </style>
